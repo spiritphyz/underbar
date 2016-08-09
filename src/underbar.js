c48a363e385539fd3d0054ad36626d9630f9ee03
spiritphyz <tonyle@ocf.berkeley.edu>
2016-08-09 16:02:17 -0700

    pass tests for first()

--- src/underbar.js
+++ src/underbar.js
@@ -32,6 +32,7 @@
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
   _.first = function(array, n) {
+    return n === undefined ? array[0] : array.slice(0, n);
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
