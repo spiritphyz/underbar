03bb4a45abdc081332240cc545c8df6299456ecc
spiritphyz <tonyle@ocf.berkeley.edu>
2016-08-09 20:38:31 -0700

    implement every() in es5 and es6

--- src/underbar.js
+++ src/underbar.js
@@ -276,12 +276,24 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    iterator = iterator || _.identity;
+    return _.reduce(collection, function(memo, item) {
+      return memo && Boolean(iterator(item));
+    }, true);
+  };
+
+  /** es6 style */
+  _.every = (collection, iterator) => {
+    iterator = iterator || _.identity;
+    return _.reduce(collection, (memo, item) => memo && 
+      Boolean(iterator(item)), true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    iterator = iterator || _.identity;
   };
 
 
