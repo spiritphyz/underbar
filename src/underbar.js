609c8f776e0d0d13f38b23d1a356c4e9505e69b5
spiritphyz <tonyle@ocf.berkeley.edu>
2016-08-09 22:12:21 -0700

    implement once() in es5

--- src/underbar.js
+++ src/underbar.js
@@ -401,19 +401,19 @@
     // TIP: These variables are stored in a "closure scope" (worth researching),
     // so that they'll remain available to the newly-generated function every
     // time it's called.
-    var alreadyCalled = false;
-    var result;
 
     // TIP: We'll return a new function that delegates to the old one, but only
     // if it hasn't been called before.
-    return function() {
-      if (!alreadyCalled) {
         // TIP: .apply(this, arguments) is the standard way to pass on all of the
         // infromation from one function call to another.
+      // The new function always returns the originally computed result.
+    var alreadyCalled = false;
+    var result;
+    return function() {
+      if (!alreadyCalled) {
         result = func.apply(this, arguments);
         alreadyCalled = true;
       }
-      // The new function always returns the originally computed result.
       return result;
     };
   };
